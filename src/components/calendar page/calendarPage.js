20d1e2217031a2d3810f336fe34258d2f4c9c49a
szimex <simon.sopchack@gmail.com>
2020-02-18 00:38:00 +0100

    Adding first hooks: managing calendar pages back and forward

--- src/components/calendar page/calendarPage.js
+++ src/components/calendar page/calendarPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './calendarPage.css';
 
 function CalendarPage() {
@@ -6,11 +6,12 @@ function CalendarPage() {
     const actualDate = new Date();
     const actualYear = actualDate.getFullYear();
     const actualMonth = actualDate.getMonth();
+    const actualDay = actualDate.getDate();
 
-    let selectedYear = 2020;
-    let selectedMonth = 2;
-    let selectedMonthcount = selectedMonth + 1;
-
+    let [selectedYear, setSelectedYear] = useState(actualYear);
+    let [selectedMonth, setSelectedMonth] = useState(actualMonth);
+    let selectedMonthShift = selectedMonth + 1;
+    
     const lastDayOfMounth = (month, year) => new Date(year, month, 0).getDate();
     const firstDayOfMounth = new Date(selectedYear, selectedMonth, 1).getDay();
     const daySwitch = (day) => day === 0 ? 6 : day -1; //offset of the days of the month from the week days table and handling sunday the first cases
@@ -22,28 +23,55 @@ function CalendarPage() {
             if (tableCells[i] < 1) {    
                 tableCells[i] = null
             }
-            if (tableCells[i] > lastDayOfMounth(selectedMonthcount, selectedYear)) {
+            if (tableCells[i] > lastDayOfMounth(selectedMonthShift, selectedYear)) {
                 tableCells[i] = null
             }
+            if (tableCells[i] === actualDay) {
+                console.log('Today is ' + tableCells[i])
+            }
         }
+    }   
+
+    const monthBack = () => {
+        if(selectedMonth > 0) {
+                setSelectedMonth(selectedMonth -1)
+            } else {
+                setSelectedMonth(11);
+                setSelectedYear(selectedYear -1);
+            }
     }
+    const monthForward = () => {
+        if (selectedMonth < 11) {
+            setSelectedMonth(selectedMonth + 1)
+        } else {
+            setSelectedMonth(0);
+            setSelectedYear(selectedYear + 1);
+        } 
+    }
+
+
     tableSetter();
+
     return (
         <table>
             <thead>
                 <tr className="table_head">
-                    <th colSpan='7'>{monthName[selectedMonth] + " " + selectedYear}</th>
+                    <td className="left_triangle"><span onClick={monthBack} className="month_back"></span></td>
+                    <th colSpan='7'>
+                        <span className="month_and_year">{monthName[selectedMonth] + " " + selectedYear}</span>
+                    </th>
+                    <td className="right_triangle"><span onClick={monthForward} className="month_forward"></span></td>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th>Pon</th>
+                    <th id="red">Pon</th>
                     <th>Wt</th>
                     <th>Śr</th>
                     <th>Czw</th>
                     <th>Pt</th>
-                    <th>So</th>
-                    <th>Nd</th>
+                    <th className="weekend">So</th>
+                    <th className="weekend">Nd</th>
                 </tr>
                 <tr>
                     <td>{tableCells[0]}</td>
--- src/components/today/today.js
+++ src/components/today/today.js
@@ -3,10 +3,10 @@ import './today.css';
 
 function Today () {
     const actualDate = new Date();
+    const actualYear = actualDate.getFullYear();
     const monthName = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'];
     const actualMonth = actualDate.getMonth();
     const actualDay = actualDate.getDate();
-    const actualYear = actualDate.getFullYear();
 
     return (
         <div>
